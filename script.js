023630f71894e528df9429612c78a31cc9ad4a2a
milosn99 <48392694+milosn99@users.noreply.github.com>
2022-03-05 17:51:38 +0100

    Changing triangles to color, other minor refactors

--- script.js
+++ script.js
@@ -1,40 +1,68 @@
-let tacka0 = {
+//Definisanje pocetnih tacaka
+const tacka0 = {
   x: 300,
   y: 600 - 519.615242271,
 };
 
-let tacka1 = {
+const tacka1 = {
   x: 0,
   y: 600,
 };
 
-let tacka2 = {
+const tacka2 = {
   x: 600,
   y: 600,
 };
 
+//Definisanje boja zbog vizualizacije
+const boje = [
+  "blue",
+  "lightblue",
+  "green",
+  "yellow",
+  "red",
+  "purple",
+  "pink",
+  "black",
+  "white",
+  "gray",
+];
+
+//Definisanje velicine kanvasa i broja iteracija
+const kontekst = {
+  sirina: 600,
+  visina: 600,
+  maxIteracija: 10,
+  minIteracija: 0,
+};
+
+//Definisanje listerenera za promjenu broja iteracija
 document
   .getElementById("number")
   .addEventListener("change", handleChangeBrojIteracija);
 
 function handleChangeBrojIteracija(e) {
   let brojIteracija = document.getElementById("number").value;
-  iscrtaj(brojIteracija);
+  resenje(brojIteracija);
 }
 
-function iscrtaj(brojIteracija) {
+//funkcija za iscrtavanje
+function resenje(brojIteracija) {
   let c = document.getElementById("canvas");
   let ctx = c.getContext("2d");
-  ctx.clearRect(0, 0, 600, 600);
+  ctx.canvas.width = kontekst.sirina;
+  ctx.canvas.height = kontekst.visina;
 
-  if (brojIteracija < 0) brojIteracija = 0;
-  if (brojIteracija > 10) brojIteracija = 10;
+  if (brojIteracija < kontekst.minIteracija)
+    brojIteracija = kontekst.minIteracija;
+  if (brojIteracija > kontekst.maxIteracija)
+    brojIteracija = kontekst.maxIteracija;
 
-  nacrtajTrougao(tacka0, tacka1, tacka2);
   sierpinski(tacka0, tacka1, tacka2, brojIteracija);
 }
 
-function nacrtajTrougao(A, B, C) {
+//funkcija za crtanje jednog trougla
+function nacrtajTrougao(A, B, C, boja) {
   let c = document.getElementById("canvas");
   let ctx = c.getContext("2d");
 
@@ -43,14 +71,18 @@ function nacrtajTrougao(A, B, C) {
   ctx.lineTo(B.x, B.y);
   ctx.lineTo(C.x, C.y);
   ctx.lineTo(A.x, A.y);
-  ctx.stroke();
-
   ctx.closePath();
+
+  ctx.fillStyle = boja;
+  ctx.fill();
 }
 
+//Algoritam za kreiranje trougla Sjerpinskog za N iteracija
 function sierpinski(A, B, C, n) {
-  if (n === 0) nacrtajTrougao(tacka0, tacka1, tacka2);
-  if (n > 0) {
+  if (n >= 0) {
+    nacrtajTrougao(A, B, C, boje[n]);
+
+    //Racunanje vrhova unutrasnjeg trougla
     let AB = {
       x: (A.x + B.x) / 2,
       y: (A.y + B.y) / 2,
@@ -66,8 +98,8 @@ function sierpinski(A, B, C, n) {
       y: B.y,
     };
 
-    nacrtajTrougao(AB, BC, AC);
-    if (n > 1) {
+    //Rekurzivni poziv za gornji, levi i desni trougao
+    if (n > 0) {
       sierpinski(A, AB, AC, n - 1);
       sierpinski(AB, B, BC, n - 1);
       sierpinski(AC, BC, C, n - 1);
@@ -75,4 +107,4 @@ function sierpinski(A, B, C, n) {
   }
 }
 
-iscrtaj(0);
+resenje(0);
